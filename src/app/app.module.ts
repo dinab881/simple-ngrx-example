7c05d5e6267837e12dec7d23b3d5d9a4231fe335
user-rq <dina.bolshakova@requestum.com>
2019-01-18 11:32:01 +0200

    Initial app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarsFormComponent } from './cars-form/cars-form.component';
+import { CarComponent } from './car/car.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsFormComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
